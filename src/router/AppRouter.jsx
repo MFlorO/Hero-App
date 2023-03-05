bed8701dc3fc0eac9b936cd88235c0c406ac19a8
Flori O <floroldani@hotmail.com>
2023-03-05 15:28:40 -0300

    rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,33 @@
-import { Route, Routes } from "react-router-dom"
-import Login from "../auth/pages/Login/Login"
-import HeroesRoutes from "../heroes/routes/HeroesRoutes"
-
+import { Route, Routes } from "react-router-dom";
+import Login from "../auth/pages/Login/Login";
+import HeroesRoutes from "../heroes/routes/HeroesRoutes";
+import PrivateRoutes from "./PrivateRoutes";
 
 
 
 const AppRouter = () => {
   return (
     <Routes>
-        <Route path="login" element={<Login />}/>
-        
-        {/* Ruta de defecto */}
-        <Route path="*" element={<HeroesRoutes />}/>x
-    </Routes>
-  )
-}
+      <Route path="login" element={<Login />} />
 
-export default AppRouter
+      <Route
+        path="*"
+        element={
+          <PrivateRoutes>
+            <HeroesRoutes />
+          </PrivateRoutes>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default AppRouter;
+
+
+
+
+
+/* Ruta de defecto */
+/* <Route path="*" element={<HeroesRoutes />}/>x */
+
